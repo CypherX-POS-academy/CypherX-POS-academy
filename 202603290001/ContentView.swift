bedfe6782d79f0c568e52743bcbdd6beeb978758
Miro <kogibeom12@gmail.com>
2026-03-29 21:05:09 +0900

    Initial Commit

---
+++ 202603290001/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  202603290001
+//
+//  Created by Gibeom Ko on 3/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 202603290001/_02603290001App.swift
@@ -0,0 +1,17 @@
+//
+//  _02603290001App.swift
+//  202603290001
+//
+//  Created by Gibeom Ko on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct _02603290001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
